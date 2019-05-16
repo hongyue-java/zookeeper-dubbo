e0ca6055d17e4486d5380cb99e85568bc88f04a3
洪越 <yuecute1212@gmail.com>
2019-05-16 16:51:12 +0800

    修改dubbo实现类

--- spring-boot-dubbo/src/main/java/com/jaycekon/dubbo/service/impl/CityDubboServiceImpl.java
+++ spring-boot-dubbo/src/main/java/com/jaycekon/dubbo/service/impl/CityDubboServiceImpl.java
@@ -14,6 +14,7 @@ import com.jaycekon.dubbo.service.CityDubboService;
 public class CityDubboServiceImpl implements CityDubboService {
 
     public City findCityByName() {
+
         return new City(1L,2L,"广州","是我的故乡");
     }
 }
